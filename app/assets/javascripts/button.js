45ce6cfd22974c2e1d3fec79a9d2dffbfa36484d
Coulton Bunney <cabunney@gmail.com>
2012-05-20 16:06:23 -0700

    slider committed

---
+++ app/assets/javascripts/button.js
@@ -0,0 +1,13 @@
+<script type="text/javascript">
+$(document).ready(function(){
+$(".subbutton").hover(
+function() {
+$(this).stop().animate({"opacity": "0.3"}, "slow");
+},
+function() {
+$(this).stop().animate({"opacity": "1"}, "slow");
+
+});
+ 
+});
+</script>
