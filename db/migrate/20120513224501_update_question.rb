361ff7bfbcd37c0d34ee9590ff83b10a12f24d8c
Qian Wang <qian.wang83@gmail.com>
2012-05-21 21:20:02 -0700

    deleted files

--- db/migrate/20120513224501_update_question.rb
+++
@@ -1,9 +0,0 @@
-class UpdateQuestion < ActiveRecord::Migration
-  def up
-  	rename_column :questions, :category, :category_id
-  end
-
-  def down
-  	rename_column :questions, :category_id, :category
-  end
-end
