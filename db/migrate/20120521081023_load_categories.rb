134bd2fb0b225d0cf5dd8d2a2696aa95918ef2cd
Qian Wang <qian.wang83@gmail.com>
2012-05-28 20:53:30 -0700

    data population

--- db/migrate/20120521081023_load_categories.rb
+++ db/migrate/20120521081023_load_categories.rb
@@ -1,16 +1,13 @@
 class LoadCategories < ActiveRecord::Migration
   def up
-  	#create initial categories
-  	category1 = Category.new(:name => "Technology")
-  	category1.save(:validate => false)
-  	category2 = Category.new(:name => "Special Education")
-  	category2.save(:validate => false)
-  	category3 = Category.new(:name => "Assessment")
-  	category3.save(:validate => false)
-
+  	cats = ["Assessment","Common Core","Lesson Plans", 
+    "Literacy", "Parents",  "Professional Learning",
+    "Resources","Technology"]
+    cats.each do |c|
+      Category.create!(c)
+    end  
   end
 
   def down
-  
   end
 end
