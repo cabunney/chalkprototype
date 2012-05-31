988a91c15fb0eb6cad210b564e1002d34b4b0793
Qian Wang <qian.wang83@gmail.com>
2012-05-31 14:19:18 -0700

    Add users

--- db/migrate/20120531172943_add_more_users.rb
+++ db/migrate/20120531172943_add_more_users.rb
@@ -3,6 +3,11 @@ class AddMoreUsers < ActiveRecord::Migration
     User.create!(:username => "shwartz.daniel",:first_name => "Daniel",
     :last_name => "Schwartz", :school_name => "Stanford School of Education",
     :password => "chalk", :password_confirmation => "chalk", :email => "daniel.schwartz@stanford.edu" )
+    
+     User.create!(:username => "azad.masi",:first_name => "Masi",
+      :last_name => "Azad", :school_name => "Montclaire Elementary",
+      :password => "chalk", :password_confirmation => "chalk", :email => "azad_masi@cusdk8.org" )
+    
   end
 
   def down
